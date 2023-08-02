8f47ab1565c1e01db08dd829e577a0130d9d10d1
CaioLima42 <caio.lima@ges.inatel.br>
2023-08-02 18:08:25 -0300

    otimizando codigo de deteccao de mao

--- deteccaoMao.py
+++ deteccaoMao.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 from math import sqrt,atan2, degrees
 import time 
 import numpy as np
+#from mediapipe.python.solutions.hands import HandLandmark
 
 class point:
     def __init__(self,*arg):
@@ -23,7 +24,7 @@ class point:
 video = cv2.VideoCapture(1)
 
 hands = mp.solutions.hands
-Hands = hands.Hands()
+Hands = hands.Hands(1)
 mpDwaw = mp.solutions.drawing_utils
 before= point(0,0,0)
 distancia_total, velocidade = 0.0,0.0
@@ -39,6 +40,40 @@ while True:
     if handPoints:
         for points in handPoints:
             mpDwaw.draw_landmarks(img, points,hands.HAND_CONNECTIONS)
+            landMarks1 = handPoints[0].landmark[0]#[HandLandmark.WRIST.value]
+            landMarks2 = handPoints[0].landmark[5]#[HandLandmark.INDEX_FINGER_MCP.value]
+            landMarks3 = handPoints[0].landmark[17]#[HandLandmark.PINKY_MCP.value]
+            newpoint = point(np.float32((landMarks1.x + landMarks2.x + landMarks3.x))/3,
+                            np.float32((landMarks1.y + landMarks2.y + landMarks3.y))/3,
+                            np.float32((landMarks1.z + landMarks2.z + landMarks3.z))/3)
+            
+
+            cv2.circle(img,(int(newpoint.x * w), int(newpoint.y * h)),4,(0,0,255),cv2.FILLED)
+
+            distancia = newpoint.calculoDistancia(before)
+            angulo = newpoint.calculoAngulo(landMarks1)
+
+            tempo_decorrido = time.time() - tempo_inicial
+
+            before = newpoint
+
+            if tempo_decorrido != 0:
+                velocidade = distancia/tempo_decorrido
+            cv2.putText(img,f"Velocidade: {velocidade:.2f} m/s",(0,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),5)
+            cv2.putText(img,f"Angulo: {angulo:.2f} graus",(0,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),5)
+
+    
+    cv2.imshow('Imagem',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
+#codigo para indicar numeradores para pontos da mao usando for
+        #for points in handPoints:
+
+'''
             for id, cord in enumerate(points.landmark):
                 cx, cy = int(cord.x * w), int(cord.y * h)
                 cv2.putText(img, str(id), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
@@ -67,12 +102,4 @@ while True:
                     cv2.putText(img,f"Angulo: {angulo:.2f} graus",(0,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),5)
                 pontos.append((cx,cy))
 #cv2.circle(img,(cx,cy),15,(0,255,0),cv2.FILLED)
-
-
-    cv2.imshow('Imagem',img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video.release()
-cv2.destroyAllWindows()
-
+'''

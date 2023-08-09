8e05bca809ab9b52f08ef957aa463e43459b5835
CaioLima42 <caio.lima@ges.inatel.br>
2023-08-09 13:43:36 -0300

    comentando codigo

--- deteccaoCorpo.py
+++ deteccaoCorpo.py
@@ -7,9 +7,11 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
+#variveis usadas para selecionar os pontos que nao aparecerao na tela
 custom_style = mp_drawing_styles.get_default_pose_landmarks_style()
 custom_connections = list(mp_pose.POSE_CONNECTIONS)
 
+#pontos do corpo que nao serao capturados
 excluded_landmarks = [
     PoseLandmark.LEFT_EYE, 
     PoseLandmark.RIGHT_EYE, 
@@ -31,9 +33,9 @@ excluded_landmarks = [
     ]
 
 for landmark in excluded_landmarks:
-    # we change the way the excluded landmarks are drawn
+    #mudamos a forma como os pontos de referência excluídos são desenhados
     custom_style[landmark] = DrawingSpec(color=(255,255,0), thickness=None) 
-    # we remove all connections which contain these landmarks
+    #removemos todas as conexões que contêm esses pontos de referência
     custom_connections = [connection_tuple for connection_tuple in custom_connections 
                             if landmark.value not in connection_tuple]
     
@@ -44,40 +46,43 @@ pose = mp_pose.Pose(
 video = cv2.VideoCapture(0)
 
 while True:
+    #captura de imagem
     success, img = video.read()
     img = cv2.flip(img, 1)
     frameRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    h, w, _ = img.shape
+    h, w, _ = img.shape # pegando proporcoes da tela
 
     try:
+        #gerando imagem com pontos selecionados
         results = pose.process(frameRGB)
         posePoints = results.pose_landmarks
 
+        mp_drawing.draw_landmarks(
+            img,
+            results.pose_landmarks,
+            connections = custom_connections, #  passing the modified connections list
+            landmark_drawing_spec=custom_style) # and drawing styledc
+
+    except:
+        continue
+
+    cv2.imshow('Image', img) #gerando aba que mostra a captura da camera 
+    if cv2.waitKey(1) & 0xFF == ord('q'):#condicao para parar a captura
+        break
+
+#encerrando a captura 
+video.release()
+cv2.destroyAllWindows()
+
+        #colocar dentro do try{
         #pegando pontos para futuramente calcular angulo e velocidade
         #landMarks[PoseLandmark.RIGHT_SHOULDER.value].x
         #landMarks = posePoints.landmark 
 
         #captura convencional de pontos
         #mp_drawing.draw_landmarks(img, posePoints, mp_pose.POSE_CONNECTIONS)
-
-        mp_drawing.draw_landmarks(
-            img,
-            results.pose_landmarks,
-            connections = custom_connections, #  passing the modified connections list
-            landmark_drawing_spec=custom_style) # and drawing style
-
+        #}
         #numerando pontos 
         #for id, cord in enumerate(posePoints.landmark):
             #cx, cy = int(cord.x * w), int(cord.y * h)
             #cv2.putText(img, str(id), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-    except:
-        continue
-
-
-    cv2.imshow('Image', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video.release()
-cv2.destroyAllWindows()
--- deteccaoMao.py
+++ deteccaoMao.py
@@ -5,7 +5,10 @@ import time
 import numpy as np
 #from mediapipe.python.solutions.hands import HandLandmark
 
+#classe para calculo de distancia e angulo a partir de 2 pontos
 class point:
+    #construtor com possibidade de entrada com 1 parametro(ponto com posicoes x,y,z) ou
+    #com 3 valores de cordenadas(x,y,z) 
     def __init__(self,*arg):
         if len(arg) == 3:
             self.x, self.y, self.z = arg[0],arg[1],arg[2]
@@ -21,54 +24,78 @@ class point:
         radian_angle = atan2(dy, dxz)
         return degrees(radian_angle)
 
-video = cv2.VideoCapture(1)
+video = cv2.VideoCapture(0) # capturando o  video na "camera 0 "
 
+#execultando captura de maos
 hands = mp.solutions.hands
-Hands = hands.Hands(1)
+Hands = hands.Hands(1) #o numero dentro dos parenteses representa quantas maos o codigo ira capturar
 mpDwaw = mp.solutions.drawing_utils
-before= point(0,0,0)
-distancia_total, velocidade = 0.0,0.0
+
+before= point(0,0,0) #iniciando aptura na posicao 0
+velocidade = 0.0 #iniciando valor de velocidade como 0
+velocidadeMedia = []#lista usada para calcular a velocidade media da mao
 while True:
+    #porcessamento de imagem continua
     success, img = video.read()
     img = cv2.flip(img,1)
     frameRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    results = Hands.process(frameRGB)
-    handPoints = results.multi_hand_landmarks
-    h, w, _ = img.shape
-    tempo_inicial = time.time()
-    pontos = []
+    results = Hands.process(frameRGB) #porcessando imagem para geracao dos pontos
+    handPoints = results.multi_hand_landmarks #gerando cordenadas dos pontos 
+
+    h, w, _ = img.shape #pegando as proporcoes de pixel da tela
+    tempo_inicial = time.time()#tempo inicial para calculo de velocidade
+
     if handPoints:
         for points in handPoints:
-            mpDwaw.draw_landmarks(img, points,hands.HAND_CONNECTIONS)
-            landMarks1 = points.landmark[0]#[HandLandmark.WRIST.value]
-            landMarks2 = points.landmark[5]#[HandLandmark.INDEX_FINGER_MCP.value]
-            landMarks3 = points.landmark[17]#[HandLandmark.PINKY_MCP.value]
+            #mpDwaw.draw_landmarks(img, points,hands.HAND_CONNECTIONS) -> desenhando todos os pontos da mao
+            landMarks1 = points.landmark[0]#[HandLandmark.WRIST.value] -> acesso ao ponto de modo mais legivel
+            landMarks2 = points.landmark[5]#[HandLandmark.INDEX_FINGER_MCP.value] -> acesso ao ponto de modo mais legivel
+            landMarks3 = points.landmark[17]#[HandLandmark.PINKY_MCP.value] -> acesso ao ponto de modo mais legivel
+            #criando um novo ponto no centro da mao
             newpoint = point(np.float32((landMarks1.x + landMarks2.x + landMarks3.x))/3,
                             np.float32((landMarks1.y + landMarks2.y + landMarks3.y))/3,
                             np.float32((landMarks1.z + landMarks2.z + landMarks3.z))/3)
             
-
+            #desenhando o novo ponto
             cv2.circle(img,(int(newpoint.x * w), int(newpoint.y * h)),4,(0,0,255),cv2.FILLED)
 
+            #calculo de distancia e angulo
             distancia = newpoint.calculoDistancia(before)
             angulo = newpoint.calculoAngulo(landMarks1)
 
-            tempo_decorrido = time.time() - tempo_inicial
+            tempo_decorrido = time.time() - tempo_inicial #tempo final para calculo de velocidade
 
-            before = newpoint
+        #condicoes para calculo de velocidade
+        if before is not None:
+            distancia = newpoint.calculoDistancia(before)
+            if tempo_decorrido > 0:
+                velocidade = distancia / tempo_decorrido
+            else:
+                velocidade = 0.0
+        else:
+            velocidade = 0.0
 
-            if tempo_decorrido != 0:
-                velocidade = distancia/tempo_decorrido
-            cv2.putText(img,f"Velocidade: {velocidade:.2f} m/s",(0,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),5)
-            cv2.putText(img,f"Angulo: {angulo:.2f} graus",(0,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),5)
+        before = newpoint #salvando ponto central da mao para calculo de distanca
 
-    
-    cv2.imshow('Imagem',img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        #plotando informacoes na tela
+        cv2.putText(img,f"Velocidade: {velocidade:.2f} m/s",(0,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),5)
+        cv2.putText(img,f"Angulo: {angulo:.2f} graus",(0,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),5)
+
+        #salvando informacoes de velocidade
+        velocidadeMedia.append(velocidade)
+
+       
+    cv2.imshow('Imagem',img)# gerando tela com a captura da camera
+    if cv2.waitKey(1) & 0xFF == ord('q'): #codicao para parar a capura
         break
 
+#fechando dela de captura
 video.release()
 cv2.destroyAllWindows()
+
+#calculo medio de velocidade
+velocidadeMedia = np.mean(velocidadeMedia)
+print(f"Velocidade média: {velocidadeMedia:.2f} m/s")
 
 #codigo para indicar numeradores para pontos da mao usando for
         #for points in handPoints:

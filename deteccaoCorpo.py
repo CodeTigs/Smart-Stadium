9e16c4080c89449e03e42d4c058d76ddb431cb47
CaioLima42 <caio.lima@ges.inatel.br>
2023-12-18 19:41:11 -0300

    Update deteccao corpo

--- deteccaoCorpo.py
+++ deteccaoCorpo.py
@@ -2,14 +2,41 @@ import cv2
 import mediapipe as mp
 from mediapipe.python.solutions.pose import PoseLandmark
 from mediapipe.python.solutions.drawing_utils import DrawingSpec
+import time
+import numpy as np
+from math import sqrt,atan2, degrees
+
+class point:
+    #construtor com possibidade de entrada com 1 parametro(ponto com posicoes x,y,z) ou
+    #com 3 valores de cordenadas(x,y,z) 
+    def __init__(self,*arg):
+        if len(arg) == 3:
+            self.x, self.y, self.z = arg[0],arg[1],arg[2]
+        else:
+            self.x, self.y, self.z = np.float32(arg[0].x), np.float32(arg[0].y), np.float32(arg[0].z)
+
+    def calculoDistancia(self,point):
+        return sqrt((self.x-point.x)**2 + (self.y-point.y)**2 + (self.z-point.z)**2)
+    
+    def calculoAngulo(self, point):
+        dy = abs(point.y - self.y)
+        dxz = sqrt((self.x - point.x) ** 2 + (self.z - point.z) ** 2)
+        radian_angle = atan2(dy, dxz)
+        return degrees(radian_angle)
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
+before= point(0,0,0) #iniciando aptura na posicao 0
+velocidade = 0.0 #iniciando valor de velocidade como 0
+velocidadeMedia = []#lista usada para calcular a velocidade media da mao
+
 #variveis usadas para selecionar os pontos que nao aparecerao na tela
 custom_style = mp_drawing_styles.get_default_pose_landmarks_style()
 custom_connections = list(mp_pose.POSE_CONNECTIONS)
+
+
 
 #pontos do corpo que nao serao capturados
 excluded_landmarks = [
@@ -32,12 +59,10 @@ excluded_landmarks = [
     PoseLandmark.LEFT_THUMB
     ]
 
-for landmark in excluded_landmarks:
-    #mudamos a forma como os pontos de referência excluídos são desenhados
-    custom_style[landmark] = DrawingSpec(color=(255,255,0), thickness=None) 
-    #removemos todas as conexões que contêm esses pontos de referência
+for landmark in excluded_landmarks:    
+    custom_style[landmark] = DrawingSpec(color=(255,255,0), thickness=None) #mudando a forma como os pontos de referência excluídos são desenhados  
     custom_connections = [connection_tuple for connection_tuple in custom_connections 
-                            if landmark.value not in connection_tuple]
+                            if landmark.value not in connection_tuple] #removendo todas as conexões que contêm esses pontos de referência
     
 pose = mp_pose.Pose(
     min_detection_confidence=0.5,
@@ -51,7 +76,7 @@ while True:
     img = cv2.flip(img, 1)
     frameRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     h, w, _ = img.shape # pegando proporcoes da tela
-
+    tempo_inicial = time.time()#tempo inicial para calculo de velocidade
     try:
         #gerando imagem com pontos selecionados
         results = pose.process(frameRGB)
@@ -61,7 +86,27 @@ while True:
             img,
             results.pose_landmarks,
             connections = custom_connections, #  passing the modified connections list
-            landmark_drawing_spec=custom_style) # and drawing styledc
+            landmark_drawing_spec=custom_style) # and drawing styledc´
+
+        newpoint = point(posePoints.landmark[30])
+        distancia = newpoint.calculoDistancia(before)
+
+        tempo_decorrido = time.time() - tempo_inicial #tempo final para calculo de velocidade
+
+        #condicoes para calculo de velocidade
+        if before is not None:
+            distancia = newpoint.calculoDistancia(before)
+            if tempo_decorrido > 0:
+                velocidade = distancia / tempo_decorrido
+            else:
+                velocidade = 0.0
+        else:
+            velocidade = 0.0
+
+        cv2.putText(img,f"Velocidade: {velocidade:.2f} m/s",(0,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),5)
+        before = newpoint #salvando ponto central da mao para calculo de distanca  
+        velocidadeMedia.append(velocidade)
+
 
     except:
         continue
@@ -73,6 +118,8 @@ while True:
 #encerrando a captura 
 video.release()
 cv2.destroyAllWindows()
+velocidadeMedia = np.mean(velocidadeMedia)
+print(f"Velocidade média: {velocidadeMedia:.2f} m/s")
 
         #colocar dentro do try{
         #pegando pontos para futuramente calcular angulo e velocidade
